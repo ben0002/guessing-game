ca0727b5dcb052d49a7cb938fa3eaaeb99af6fcb
Benny <bennyweng520@gmail.com>
2024-08-07 20:34:34 -0400

    added enum for mode and diff

---
+++ GuessingGame/src/Difficulty.java
@@ -0,0 +1,5 @@
+public enum Difficulty {
+    EASY, // Number generated will be from 1 - 50 
+    MEDIUM, // Number generated will be from 1 - 500
+    HARD; // Number generated will be from 1 - 1000
+}
---
+++ GuessingGame/src/Mode.java
@@ -0,0 +1,5 @@
+public enum Mode {
+    NORMAL, 
+    TIMELIMIT,
+    GUESSLIMIT,
+}
